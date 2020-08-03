8d07d937e594e31b7713c45e3ef4606a69a974f0
Neel278 <neelthakkar2001@gmail.com>
2020-08-03 16:46:13 +0530

    starting to develop a chat application for interaction between two users

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -8,7 +8,7 @@ Rangrezz | Auctions
 <section class="content">
     <div class="container-fluid">
         <div class="block-header">
-            <h2>DASHBOARD</h2>
+            <h2>PROFILE</h2>
         </div>
 
         <!-- Body Copy -->
@@ -101,6 +101,8 @@ Rangrezz | Auctions
                             <ul class="nav nav-tabs" role="tablist">
                                 <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab"
                                         data-toggle="tab">Recent Paintings</a></li>
+                                <li role="presentation"><a href="#profile_settings" aria-controls="settings" role="tab"
+                                        data-toggle="tab">Profile Settings</a></li>
                             </ul>
                             <div class="tab-content">
                                 <div role="tabpanel" class="tab-pane fade in active" id="home">
@@ -165,6 +167,67 @@ Rangrezz | Auctions
                                     @endforelse
                                     {{-- --------------------------------------------------------------- --}}
                                 </div>
+                                {{-- ======================================================================== --}}
+                                <div role="tabpanel" class="tab-pane fade in" id="profile_settings">
+                                    <form class="form-horizontal">
+                                        <div class="form-group">
+                                            <label for="NameSurname" class="col-sm-2 control-label">Name Surname</label>
+                                            <div class="col-sm-10">
+                                                <div class="form-line">
+                                                    <input type="text" class="form-control" id="NameSurname"
+                                                        name="NameSurname" placeholder="Name Surname"
+                                                        value="Marc K. Hammond" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="Email" class="col-sm-2 control-label">Email</label>
+                                            <div class="col-sm-10">
+                                                <div class="form-line">
+                                                    <input type="email" class="form-control" id="Email" name="Email"
+                                                        placeholder="Email" value="example@example.com" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="InputExperience"
+                                                class="col-sm-2 control-label">Experience</label>
+
+                                            <div class="col-sm-10">
+                                                <div class="form-line">
+                                                    <textarea class="form-control" id="InputExperience"
+                                                        name="InputExperience" rows="3"
+                                                        placeholder="Experience"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="InputSkills" class="col-sm-2 control-label">Skills</label>
+
+                                            <div class="col-sm-10">
+                                                <div class="form-line">
+                                                    <input type="text" class="form-control" id="InputSkills"
+                                                        name="InputSkills" placeholder="Skills">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-sm-10">
+                                                <input type="checkbox" id="terms_condition_check"
+                                                    class="chk-col-red filled-in" />
+                                                <label for="terms_condition_check">I agree to the <a href="#">terms and
+                                                        conditions</a></label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-sm-10">
+                                                <button type="submit" class="btn btn-danger">SUBMIT</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                {{-- ================================================================ --}}
                             </div>
                         </div>
                     </div>
